072b8fbbd192ed85903ba5791ad60f428f31a0a0
oleg <martelskiy@gmail.com>
2024-01-22 17:54:22 +0100

    Remove TS related config, install ethers.js

---
+++ src/hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-truffle5");
+require("dotenv-defaults/config");
+
+const accounts = require("./hardhatAccountsList2k.js");
+const accountsList = accounts.accountsList;
+
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.23",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100
+          }
+        }
+      },
+      {
+        version: "0.5.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100
+          }
+        }
+      },
+      {
+        version: "0.6.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ]
+  },
+  networks: {
+    hardhat: {
+      accounts: accountsList,
+      initialBaseFeePerGas: 0
+    }
+  }
+};
