add8eb7ed43518f0f70e2c99120dd4be9e08bb85
oleg <martelskiy@gmail.com>
2024-01-23 09:31:23 +0100

    Move hardhatAccountList2k.js under 'utils' directory

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -2,9 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-truffle5");
 require("dotenv-defaults/config");
 
-const accounts = require("./hardhatAccountsList2k.js");
+const accounts = require("./utils/hardhatAccountsList2k.js");
 const accountsList = accounts.accountsList;
-
 
 module.exports = {
   solidity: {
@@ -14,34 +13,34 @@ module.exports = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 100
-          }
-        }
+            runs: 100,
+          },
+        },
       },
       {
         version: "0.5.17",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 100
-          }
-        }
+            runs: 100,
+          },
+        },
       },
       {
         version: "0.6.11",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200
-          }
-        }
-      }
-    ]
+            runs: 200,
+          },
+        },
+      },
+    ],
   },
   networks: {
     hardhat: {
       accounts: accountsList,
-      initialBaseFeePerGas: 0
-    }
-  }
+      initialBaseFeePerGas: 0,
+    },
+  },
 };
