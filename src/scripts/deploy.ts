0d5e845f0e333e984f4581cde26d84e34c6cabfb
Oleg <martelskiy@gmail.com>
2024-04-22 18:01:45 +0200

    Implement deploy and initialize scripts

---
+++ src/scripts/deploy.ts
@@ -0,0 +1,54 @@
+import { ethers, network } from "hardhat";
+import { State } from "../utils/deploy/state";
+import { Deployer } from "../utils/deploy/deployer";
+import { Verifier } from "../utils/deploy/verifier";
+import { configurationParameters } from "./parameters/hardhat";
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+
+  console.log(
+    `starting the deployment script, will deploy contracts to the network: '${network.name}', 
+     with owner set to: '${signer.address}'`,
+  );
+
+  const state = new State(`./scripts/state/${network.name}.json`);
+  const deployer = new Deployer(
+    configurationParameters.txConfirmations,
+    configurationParameters.gasPriceWei,
+  );
+  const verifier = new Verifier(configurationParameters.etherscanURL);
+  const deploymentState = await state.load();
+
+  for (const { name, ctorArguments } of configurationParameters.contracts) {
+    if (deploymentState.has(name)) {
+      console.log(
+        `contract with name ${name} was already deployed. Skipping...`,
+      );
+      continue;
+    }
+    try {
+      const contract = await deployer.deploy(name, ctorArguments);
+      const contractAddress = await contract.getAddress();
+      const verification = await verifier.verifyAddress(contractAddress);
+
+      deploymentState.set(name, {
+        address: contractAddress,
+        txHash: contract.deploymentTransaction()?.hash!,
+        verification: verification,
+      });
+      await state.save(deploymentState);
+    } catch (error) {
+      let errorMessage: string = "";
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.log(
+        `error during deployment contract: '${name}'. Error: '${errorMessage}'. Continue...`,
+      );
+      continue;
+    }
+  }
+}
+
+main();
---
+++ src/scripts/initialize.ts
@@ -0,0 +1,46 @@
+import { ethers, network } from "hardhat";
+import { State } from "../utils/deploy/state";
+import { Initializer } from "../utils/initialize/initializer";
+import { configurationParameters } from "./parameters/hardhat";
+import { ContractName } from "../utils/deploy/types";
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+
+  console.log(
+    `starting the initialization script, will initialize contracts at the network: '${network.name}', 
+     with owner set to: '${signer.address}'`,
+  );
+
+  const state = new State(`./scripts/state/${network.name}.json`);
+  const contractTransactions = await state.load();
+
+  console.log(
+    `state is loaded. Number of items in the state: ${contractTransactions.size}`,
+  );
+
+  const initializer: Initializer = new Initializer(
+    configurationParameters.gasPriceWei,
+    configurationParameters.txConfirmations,
+  );
+
+  const contractsMap = new Map<ContractName, string>();
+  for (const [name, transaction] of contractTransactions) {
+    contractsMap.set(name as ContractName, transaction.address);
+  }
+  const connectedContracts = await initializer.connect(signer, contractsMap);
+
+  await initializer.initialize(
+    connectedContracts,
+    configurationParameters.collaterals,
+    configurationParameters.internalAddresses.GOVERNANCE,
+    configurationParameters.internalAddresses.OATH,
+    configurationParameters.internalAddresses.TREASURY,
+    configurationParameters.externalAddresses.SWAPPER,
+    configurationParameters.externalAddresses.VELO_ROUTER,
+    configurationParameters.externalAddresses.BALANCER_VAULT,
+    configurationParameters.externalAddresses.UNI_V3_ROUTER,
+  );
+}
+
+main();
