3118f9e6eb97cf990658a163e9540a0b88fe9748
Oleg <martelskiy@gmail.com>
2024-04-22 18:13:38 +0200

    Add separate hardhat config file for tests

--- src/hardhat.config.test.ts
+++ src/hardhat.config.test.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
 import "@nomiclabs/hardhat-truffle5";
 import "dotenv-defaults/config";
+import { accountsList } from "./utils/accountsList.js";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -15,6 +16,12 @@ const config: HardhatUserConfig = {
         version: "0.4.23",
       },
     ],
+  },
+  networks: {
+    hardhat: {
+      accounts: accountsList,
+      initialBaseFeePerGas: 0, //this setting is needed for some of tests to work
+    },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
