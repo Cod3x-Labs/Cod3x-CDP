83f1dd726b56deef1f15cbe53b65092f07f08264
Oleg <martelskiy@gmail.com>
2024-04-23 15:42:14 +0200

    Remove misleading comment from AggregatorV3Interface.sol file

--- src/contracts/Dependencies/AggregatorV3Interface.sol
+++ src/contracts/Dependencies/AggregatorV3Interface.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: BUSL-1.1
-// Code from https://github.com/smartcontractkit/chainlink/blob/master/evm-contracts/src/v0.6/interfaces/AggregatorV3Interface.sol
 
 pragma solidity ^0.8.23;
 
